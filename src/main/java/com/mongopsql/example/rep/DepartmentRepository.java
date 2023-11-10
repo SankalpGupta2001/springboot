9d93cddddfe0a4c684db5f1d00cf699122559381
SankalpGupta2001 <83775048+SankalpGupta2001@users.noreply.github.com>
2023-11-10 12:47:09 +0530

    Delete src directory

--- src/main/java/com/mongopsql/example/rep/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mongopsql.example.rep;
-
-import com.mongopsql.example.models.Department;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DepartmentRepository extends MongoRepository<Department,String> {
-}
--- src/main/java/com/mongopsql/example/rep/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mongopsql.example.rep;
-
-import com.mongopsql.example.models.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee,Long> {
-}
